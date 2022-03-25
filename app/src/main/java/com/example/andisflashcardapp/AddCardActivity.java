04e7443f5d46f985ec4d9229d1e5267a35ddf699
AKolari <90071560+AKolari@users.noreply.github.com>
2022-03-25 19:25:12 -0400

    This is my Fourth version of the flashcard app. I added all of the optional features from lab one, which I did not initially include, and I added all mandatory requirements for lab three, and some of the optional requirements.

--- app/src/main/java/com/example/andisflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/andisflashcardapp/AddCardActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -14,11 +16,26 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+        String question = getIntent().getStringExtra("Question");
+        String correct_Answer = getIntent().getStringExtra("Answer");
+        String incorrect_Answer1 = getIntent().getStringExtra("Incorrect1");
+        String incorrect_Answer2 = getIntent().getStringExtra("Incorrect2");
+
+        if(question!=null){
+
+            ((EditText)findViewById(R.id.editQuestion)).setText(question);
+            ((EditText)findViewById(R.id.editAnswer)).setText(correct_Answer);
+            ((EditText)findViewById(R.id.editAnswer1)).setText(incorrect_Answer1);
+            ((EditText)findViewById(R.id.editAnswer2)).setText(incorrect_Answer2);
+        }
+
+
         findViewById(R.id.flashcard_add_cancel).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
+
         });
 //ALTERNATE: findViewById(R.id.flashcard_add_cancel).setOnClickListener(v -> finish());
 
@@ -38,8 +55,10 @@ public class AddCardActivity extends AppCompatActivity {
                 String correct_Answer= ((EditText) findViewById(R.id.editAnswer)).getText().toString();
                 String incorrect_Answer1= ((EditText) findViewById(R.id.editAnswer1)).getText().toString();
                 String incorrect_Answer2= ((EditText) findViewById(R.id.editAnswer2)).getText().toString();
-
-
+        if(question.length()==0||correct_Answer.length()==0||incorrect_Answer2.length()==0||incorrect_Answer1.length()==0){
+            Toast.makeText(getApplicationContext(),"Fill all fields",Toast.LENGTH_SHORT).show();
+        }
+else{
                 Intent data =new Intent();
                 data.putExtra("Question",question );
                 data.putExtra("Answer",correct_Answer );
@@ -47,6 +66,7 @@ public class AddCardActivity extends AppCompatActivity {
                 data.putExtra("Incorrect2",incorrect_Answer2 );
                 setResult(RESULT_OK, data);
                 finish();
+}
             }
         });
 
--- app/src/main/java/com/example/andisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/andisflashcardapp/MainActivity.java
@@ -9,26 +9,40 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    boolean answers_Visible=true;
+   void defaultCards(){
+       findViewById(R.id.FlashcardAnswer).setBackground(getResources().getDrawable(R.drawable.answer_background_neutral));
+       findViewById(R.id.FlashcardAnswer1).setBackground(getResources().getDrawable(R.drawable.answer_background_neutral));
+       findViewById(R.id.FlashcardAnswer2).setBackground(getResources().getDrawable(R.drawable.answer_background_neutral));
 
+   }
 
-
-
-
-
-
-
-
-
-
+   boolean answers_Visible=true;
+   boolean options_visible=false;
+FlashcardDatabase database;
+List<Flashcard> cards;
+    int cardIndex=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database=new FlashcardDatabase(getApplicationContext());
+
+        cards=database.getAllCards();
+findViewById(R.id.flashcard_delete).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_add).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_edit).setVisibility(View.INVISIBLE);
+
+        ((TextView)findViewById(R.id.FlashcardQuestion)).setText(cards.get(cardIndex).getQuestion());
+        ((TextView)findViewById(R.id.FlashcardAnswer)).setText(cards.get(cardIndex).getAnswer());
+        ((TextView)findViewById(R.id.FlashcardAnswer1)).setText(cards.get(cardIndex).getWrongAnswer1());
+        ((TextView)findViewById(R.id.FlashcardAnswer2)).setText(cards.get(cardIndex).getWrongAnswer2());
 /* findViewById(R.id.FlashcardQuestion).setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -73,6 +87,7 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
     @Override
     public void onClick(View view) {
         if (answers_Visible) {
+            defaultCards();
             findViewById(R.id.FlashcardAnswer).setVisibility(View.INVISIBLE);
             findViewById(R.id.FlashcardAnswer1).setVisibility(View.INVISIBLE);
             findViewById(R.id.FlashcardAnswer2).setVisibility(View.INVISIBLE);
@@ -80,6 +95,7 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
             answers_Visible=false;
 
         } else {
+            defaultCards();
             findViewById(R.id.FlashcardAnswer).setVisibility(View.VISIBLE);
             findViewById(R.id.FlashcardAnswer1).setVisibility(View.VISIBLE);
             findViewById(R.id.FlashcardAnswer2).setVisibility(View.VISIBLE);
@@ -89,15 +105,6 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
     }
 } );
 
-
-
-
-
-
-
-
-
-
         findViewById(R.id.flashcard_add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -106,6 +113,46 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
 
             }
         });
+        findViewById(R.id.flashcard_edit).setOnClickListener((View v) -> {
+
+
+
+            String question= ((TextView) findViewById(R.id.FlashcardQuestion)).getText().toString();
+            String correct_Answer= ((TextView) findViewById(R.id.FlashcardAnswer)).getText().toString();
+            String incorrect_Answer1= ((TextView) findViewById(R.id.FlashcardAnswer1)).getText().toString();
+            String incorrect_Answer2= ((TextView) findViewById(R.id.FlashcardAnswer2)).getText().toString();
+
+            Intent current =new Intent(MainActivity.this, AddCardActivity.class);
+            current.putExtra("Question",question );
+            current.putExtra("Answer",correct_Answer );
+            current.putExtra("Incorrect1",incorrect_Answer1 );
+            current.putExtra("Incorrect2",incorrect_Answer2 );
+
+            MainActivity.this.startActivityForResult(current, 200);
+        });
+
+
+        findViewById(R.id.flashcard_delete).setOnClickListener((View v) -> {
+    database.deleteCard(((TextView) findViewById(R.id.FlashcardQuestion)).getText().toString());
+    cards=database.getAllCards();
+            cardIndex--;
+            if(cardIndex<=-1){
+                cardIndex=cards.size()-1;
+            }
+
+            if(cards!=null && cards.size()>0){
+                ((TextView)findViewById(R.id.FlashcardQuestion)).setText(cards.get(cardIndex).getQuestion());
+                ((TextView)findViewById(R.id.FlashcardAnswer)).setText(cards.get(cardIndex).getAnswer());
+                ((TextView)findViewById(R.id.FlashcardAnswer1)).setText(cards.get(cardIndex).getWrongAnswer1());
+                ((TextView)findViewById(R.id.FlashcardAnswer2)).setText(cards.get(cardIndex).getWrongAnswer2());
+            }else{
+                ((TextView)findViewById(R.id.FlashcardQuestion)).setText("QUESTION");
+                ((TextView)findViewById(R.id.FlashcardAnswer)).setText("ANSWER");
+                ((TextView)findViewById(R.id.FlashcardAnswer1)).setText("WRONG");
+                ((TextView)findViewById(R.id.FlashcardAnswer2)).setText("WRONG");
+
+            }
+             });
 
 
 
@@ -113,9 +160,45 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
 
 
 
+        findViewById(R.id.flashcard_next).setOnClickListener((View v) -> {
+defaultCards();
+        cardIndex++;
+        if(cardIndex==cards.size()){
+            cardIndex=0;
+        }
+
+        if(cards!=null && cards.size()>0){
+            ((TextView)findViewById(R.id.FlashcardQuestion)).setText(cards.get(cardIndex).getQuestion());
+            ((TextView)findViewById(R.id.FlashcardAnswer)).setText(cards.get(cardIndex).getAnswer());
+            ((TextView)findViewById(R.id.FlashcardAnswer1)).setText(cards.get(cardIndex).getWrongAnswer1());
+            ((TextView)findViewById(R.id.FlashcardAnswer2)).setText(cards.get(cardIndex).getWrongAnswer2());
+        }
+
+        });
 
 
 
+
+findViewById(R.id.flashcard_options).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        if (options_visible) {
+            defaultCards();
+            findViewById(R.id.flashcard_edit).setVisibility(View.INVISIBLE);
+            findViewById(R.id.flashcard_add).setVisibility(View.INVISIBLE);
+            findViewById(R.id.flashcard_delete).setVisibility(View.INVISIBLE);
+           options_visible=false;
+
+        } else {
+            defaultCards();
+            findViewById(R.id.flashcard_edit).setVisibility(View.VISIBLE);
+            findViewById(R.id.flashcard_add).setVisibility(View.VISIBLE);
+            findViewById(R.id.flashcard_delete).setVisibility(View.VISIBLE);
+           options_visible=true;
+        }
+
+    }
+});
 
 
 
@@ -124,36 +207,43 @@ findViewById(R.id.toggle_answers).setOnClickListener(new View.OnClickListener() 
 
 
     }
-
-
-
-
 
 
    // @SuppressLint("MissingSuperCall")   Errors tells me to call super. I don't know what that means, but it still works without it
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100) {
-            String question=data.getExtras().getString("Question");
-            String correct_Answer=data.getExtras().getString("Answer");
-            String incorrect_Answer1=data.getExtras().getString("Incorrect1");
-            String incorrect_Answer2=data.getExtras().getString("Incorrect2");
+        if(resultCode==RESULT_OK) {
+            String question = data.getExtras().getString("Question");
+            String correct_Answer = data.getExtras().getString("Answer");
+            String incorrect_Answer1 = data.getExtras().getString("Incorrect1");
+            String incorrect_Answer2 = data.getExtras().getString("Incorrect2");
+            ((TextView) findViewById(R.id.FlashcardQuestion)).setText(question);
+            ((TextView) findViewById(R.id.FlashcardAnswer)).setText(correct_Answer);
+            ((TextView) findViewById(R.id.FlashcardAnswer1)).setText(incorrect_Answer1);
+            ((TextView) findViewById(R.id.FlashcardAnswer2)).setText(incorrect_Answer2);
+            if (requestCode == 100) {
 
-            ((TextView)findViewById(R.id.FlashcardQuestion)).setText(question);
-            ((TextView)findViewById(R.id.FlashcardAnswer)).setText(correct_Answer);
-            ((TextView)findViewById(R.id.FlashcardAnswer1)).setText(incorrect_Answer1);
-            ((TextView)findViewById(R.id.FlashcardAnswer2)).setText(incorrect_Answer2);
+                database.insertCard(new Flashcard(question, correct_Answer, incorrect_Answer1, incorrect_Answer2));
+                cards = database.getAllCards();
+                cardIndex = -1;
+            } else if (requestCode == 200) {
+                cards.get(cardIndex).setQuestion(question);
+                cards.get(cardIndex).setAnswer(correct_Answer);
+                cards.get(cardIndex).setWrongAnswer1(incorrect_Answer1);
+                cards.get(cardIndex).setWrongAnswer2(incorrect_Answer2);
 
-
+                database.updateCard(cards.get(cardIndex));
+            }
         }
+        defaultCards();
 
     }
 }
 
 
 /*
-Notes:
+Notes March 12th:
 view.invisible makes it invisible, but view.gone makes removes the view from the layout
 AN intent is an object that has the intention to perform some action.
  Intent intent=
@@ -169,13 +259,13 @@ AN intent is an object that has the intention to perform some action.
 protected void onCreate(Bundle savedInstanceState)  {
     String data= getIntent(). getStringExtra("some_key);
     ...
-    }   //INclude Null check
+    }   //Include Null check
 
     Allows for the transfer of data between activities
 
 
 
-
+Notes March 19th:
 
 
 
